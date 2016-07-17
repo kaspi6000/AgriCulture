24fb5b757b8528126dbe244b7c6ef62afcba813b
kang <kaspi6000@gmail.com>
2016-07-17 21:54:34 +0900

    header

---
+++ admin/header.php
@@ -0,0 +1,49 @@
+<?php
+
+    function getHeader(){
+
+        echo  "<div class = 'headercontent'>
+
+				<div class = 'header'>
+
+					<a href='index.php' class = 'logo'>LOGO</a>
+
+				</div>
+
+				<div class = 'header_list'>
+
+					<ul class = 'nav'>
+						<li>
+							<a href='' class = 'pagescroll'>phase1</a>
+                            <ul class = 'menubar'>
+                                <li><a href= ''class = ''>test1</a></li>
+                                <li><a href= ''class = ''>test2</a></li>
+                                <li><a href= ''class = ''>test3</a></li>
+                            </ul>
+						</li>
+						<li>
+							<a href='' class = 'pagescroll'>phase2</a>
+						</li>
+						<li>
+							<a href='' class = 'pagescroll'>phase3</a>
+						</li>
+						<li>
+							<a href='' class = 'pagescroll'>phase4</a>
+						</li>
+                        <li>
+							<a href='' class = 'pagescroll'>phase5</a>
+						</li>
+                        <li>
+							<a href='' class = 'pagescroll'>phase6</a>
+						</li>
+                        <li>
+							<a href='' class = 'pagescroll'>phase7</a>
+						</li>
+					</ul>
+
+				</div>
+
+				</div>";
+    }
+
+ ?>
---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<?php
+    require_once("./header.php");
+ ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <meta charset="utf-8">
+
+        <title>Korea Agirculture</title>
+
+        <link rel="stylesheet" href="bootstrap.css">
+        <!-- bootstrap.css -->
+        <link rel="stylesheet" href="index.css">
+        <!-- Custom css -->
+        <script src="jquery-1.11.3.js"></script>
+        <!-- jquery.js -->
+    </head>
+    <body>
+
+        <?php getHeader(); ?>
+
+    </body>
+</html>
