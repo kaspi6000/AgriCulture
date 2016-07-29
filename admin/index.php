c3c9b9bbdbd0302c7f538a64f394760649d43d9e
kang <kaspi6000@gmail.com>
2016-07-29 19:59:12 +0900

    Home index

--- admin/index.php
+++ admin/index.php
@@ -21,5 +21,50 @@
 
         <?php getHeader(); ?>
 
+        <!-- <ul class = "menu nav">
+            <li class = "first dropdown">
+                <a href="#" class = "dropdown-toggle">phase1</a>
+                <ul class = "outo">
+                    <li><a href="#">step1</a></li>
+                    <li><a href="#">step2</a></li>
+                    <li><a href="#">step3</a></li>
+                </ul>
+            </li>
+            <li class = "second">
+                <a href="#">phase2</a>
+            </li>
+            <li class = "third">
+                <a href="#">phase3</a>
+            </li>
+        </ul> -->
+
+        <div class="dropdown">
+            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                Dropdown
+                <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+            </ul>
+        </div>
+
+        <div class="dropup">
+            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropup
+                <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+            </ul>
+        </div>
+
     </body>
 </html>
